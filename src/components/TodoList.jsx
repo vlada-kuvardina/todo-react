f717b8e28cd302ed84cee9b490d36af984228b6f
vlada-kuvardina <vlalakuvardina@gmail.com>
2026-04-03 11:16:29 +0300

    perf: prevent unnecessary re-renders using memo, useMemo and useCallback

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,3 +1,4 @@
+import { memo } from 'react'
 import TodoItem from './TodoItem'
 
 const TodoList = (props) => {
@@ -37,4 +38,4 @@ const TodoList = (props) => {
   )
 }
 
-export default TodoList
+export default memo(TodoList)
