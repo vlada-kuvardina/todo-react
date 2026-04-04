70daf8b38ee1ab2d6d535367c247163dc59a1b3c
vlada-kuvardina <vlalakuvardina@gmail.com>
2026-04-04 19:57:46 +0300

    feat: add routing for todo detail view

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,6 @@
 import { memo, useContext } from 'react'
 import { TasksContext } from '../context/TasksContext'
+import RouterLink from './RouterLink'
 
 const TodoItem = (props) => {
   const {
@@ -31,11 +32,14 @@ const TodoItem = (props) => {
         }}
       />
       <label
-        className="todo-item__label"
+        className="todo-item__label visually-hidden"
         htmlFor={id}
       >
         {title}
       </label>
+      <RouterLink to={`/tasks/${id}`} aria-label="Task detail page">
+        {title}
+      </RouterLink>
       <button
         className="todo-item__delete-button"
         aria-label="Delete"
