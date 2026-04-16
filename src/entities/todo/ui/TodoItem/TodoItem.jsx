dc2bd011bdee690bff645c4632a55519c549a2b0
vlada-kuvardina <vlalakuvardina@gmail.com>
2026-04-16 12:13:27 +0300

    fix: remove calls to deleted functions causing runtime crash

--- src/entities/todo/ui/TodoItem/TodoItem.jsx
+++ src/entities/todo/ui/TodoItem/TodoItem.jsx
@@ -2,7 +2,6 @@ import { memo, useContext } from 'react'
 import { TasksContext } from '@/entities/todo'
 import RouterLink from '@/shared/ui/RouterLink'
 import styles from './TodoItem.module.scss'
-import {highlightCaseInsensitive} from '@/shared/utils/highlight'
 
 const TodoItem = (props) => {
   const {
@@ -19,10 +18,7 @@ const TodoItem = (props) => {
     toggleTaskComplete,
     disappearingTaskId,
     appearingTaskId,
-    searchQuery,
   } = useContext(TasksContext)
-
-  const highlightedTitle = highlightCaseInsensitive(title, searchQuery)
 
   return (
     <li
@@ -50,7 +46,7 @@ const TodoItem = (props) => {
         {title}
       </label>
       <RouterLink to={`tasks/${id}`} aria-label="Task detail page">
-        <span dangerouslySetInnerHTML={{ __html: highlightedTitle }} />
+        {title}
       </RouterLink>
       <button
         className={styles.deleteButton}
